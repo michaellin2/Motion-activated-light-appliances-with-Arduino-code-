f2759d735ee710d03e2098976f8a89905fe370e3
michaellin2 <lin1042612714@gmail.com>
2022-04-20 20:26:14 +0100

    draft2

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,22 @@
 #include <ESPmDNS.h>
 #include <ArduinoJson.h>
 #include <EEPROM.h>
- 
+#include <analogWrite.h>
+
+#include"DHT.h"
+#define tempSensorPin 27
+#define sensorPin 26
+#define red 25
+#define green 23
+#define blue 32
+uint8_t color =0;
+uint8_t brightness = 255;
+uint32_t R,G,B;
+const boolean invert =true;
+
 #define EEPROM_SIZE 12
+
+
 // const char* ssid = "BT-6ZAGW5";
 // const char* password = "PvhViqdkLc7nCR";
 const char* ssid = "10DC Hyperoptic 1Gb Fibre 2.4Ghz";
@@ -16,6 +30,90 @@ String apiName = "";
 int address = 0;
 // Serving Hello world
 
+float temperature = 0;
+DHT dht(tempSensorPin,DHT22);
+
+void RGB(uint8_t hue, uint8_t brightness)
+{
+    uint16_t scaledHue = (hue * 6);
+    uint8_t segment = scaledHue / 256; // segment 0 to 5 around the
+                                            // color wheel
+    uint16_t segmentOffset =
+      scaledHue - (segment * 256); // position within the segment
+
+    uint8_t complement = 0;
+    uint16_t prev = (brightness * ( 255 -  segmentOffset)) / 256;
+    uint16_t next = (brightness *  segmentOffset) / 256;
+
+    if(invert)
+    {
+      brightness = 255 - brightness;
+      complement = 255;
+      prev = 255 - prev;
+      next = 255 - next;
+    }
+
+    switch(segment ) {
+    case 0:      // red
+        R = brightness;
+        G = next;
+        B = complement;
+    break;
+    case 1:     // yellow
+        R = prev;
+        G = brightness;
+        B = complement;
+    break;
+    case 2:     // green
+        R = complement;
+        G = brightness;
+        B = next;
+    break;
+    case 3:    // cyan
+        R = complement;
+        G = prev;
+        B = brightness;
+    break;
+    case 4:    // blue
+        R = next;
+        G = complement;
+        B = brightness;
+    break;
+   case 5:      // magenta
+    default:
+        R = brightness;
+        G = complement;
+        B = prev;
+    break;
+    }
+}
+
+void motionDetect(){
+  if (digitalRead(sensorPin)==HIGH){
+    for(color=0;color<255;color++){
+      RGB(color,brightness);
+      ledcWrite(0,R);
+      ledcWrite(1,G);
+      ledcWrite(2,B);
+      delay(10);
+    }
+  }
+}
+
+void getTemperature(){
+  temperature = dht.readTemperature();
+  if(temperature>22){
+    ledcWrite(2,B);
+  }
+  else if(temperature<5){
+    ledcWrite(0,R);
+  }
+  else{
+    ledcWrite(0,R);
+    ledcWrite(1,G);
+    ledcWrite(2,B);
+  }
+}
 
 void setRoom() {
     String postBody = server.arg("plain");
@@ -122,6 +220,20 @@ void handleNotFound() {
  
 void setup(void) {
   Serial.begin(115200);
+  //start the temperature sensor
+  dht.begin();
+  //motion sensor
+  pinMode(sensorPin,INPUT);
+  //rgb color
+  ledcSetup(0,5000,8);
+  ledcAttachPin(red,0);
+
+  ledcSetup(1,5000,8);
+  ledcAttachPin(green,1);
+
+  ledcSetup(2,5000,8);
+  ledcAttachPin(blue,2);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -151,6 +263,8 @@ void setup(void) {
   Serial.println("HTTP server started");
 }
 
+
 void loop(void) {
   server.handleClient();
+  motionDetect();
 }
